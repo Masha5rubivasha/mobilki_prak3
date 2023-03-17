f6a5d9f7fb1342c1e37a3f3548d81108e0964c87
Sokolova Mariia <mashasokolova7@gmail.com>
2023-03-17 16:04:26 +0300

    Тосты и логи во всех этапах жизненного цикла фрагмента

--- app/src/main/java/com/example/myproject/FirstFragment.java
+++ app/src/main/java/com/example/myproject/FirstFragment.java
@@ -46,58 +46,92 @@ public class FirstFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        Toast.makeText(getContext(), "onCreate", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onCreate");
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onCreateView");
         return inflater.inflate(R.layout.fragment_first, container, false);
 
-
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
+        Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onViewCreated");
     }
 
     @Override
     public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
+
+        Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onViewStateRestored");
     }
     public void onStart() {
         super.onStart();
+
+        Button save_button = getActivity().findViewById(R.id.buttonSave);//кнопка сохранения
+
+        Toast.makeText(getContext(), "onStart", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onStart");
+
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.i (TAG, "Данные о пользователе сохранены");
+                Toast.makeText(getActivity(),"Сохранение",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
     public void onPause() {
         super.onPause();
+
+        Toast.makeText(getContext(), "onPause", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onPause");
     }
 
     @Override
     public void onStop() {
         super.onStop();
+
+        Toast.makeText(getContext(), "onStop", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onStop");
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
+        Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onSaveInstanceState");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+
+        Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onDestroyView");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        Toast.makeText(getContext(), "onDestroy", Toast.LENGTH_LONG).show();
+        Log.i("Fragment", "onDestroy");
     }
 
 }
